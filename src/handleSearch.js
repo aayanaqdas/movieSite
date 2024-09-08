262e90d46b6d7509660eb098be8a4ea315a90a68
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2024-09-09 00:50:13 +0200

    added skeleton ui to search cards. removed loader and replaced with skeleton. made it so when the final result are in it wont try to load in new ones by keeping track of how many results are available to load and how many are already loaded in.

--- src/handleSearch.js
+++ src/handleSearch.js
@@ -1,5 +1,4 @@
-import{apiKey, imgApi} from "./main.js"
-
+import { apiKey, imgApi } from "./main.js";
 
 const searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=`;
 
@@ -18,101 +17,101 @@ let isSearching = false;
 
 let searchTerm;
 
-let loader = document.getElementById("loader");
-let isLoadingMore = false;
+let totalResultsCount = 0;
+let totalSearchResults;
 
-
+const searchSkeleton = document.getElementById("search-skeleton-container");
 
 /***********Searching************/
 // Fetch JSON data from url
 async function fetchDataSearch(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Display an error message to the user
+    searchResult.innerHTML =
+      "<p>Error fetching data. Please try again later.</p>";
+    searchSkeleton.classList.add("hide-element");
+  }
+}
+
+// Fetch and show results based on url for searching
+async function fetchAndShowSearch(url) {
+  const data = await fetchDataSearch(url);
+  if (data && data.results) {
+    searchSkeleton.classList.add("hide-element");
+    showSearchResults(data.results);
+
+    //keeps track of how many results are available and how many results are currently loaded in
+    totalSearchResults = data.total_results;
+    let currentResultsLength = data.results.length;
+    totalResultsCount += currentResultsLength;
+
+    console.log(totalSearchResults);
+  }
+}
+
+function showSearchResults(item) {
+  const searchContent = item
+    .map((media) => {
+      if (media.media_type === "movie") {
+        return createSearchCard(media);
+      } else if (media.media_type === "tv") {
+        return createSearchCardTv(media);
       }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      // Display an error message to the user
-      searchResult.innerHTML =
-        "<p>Error fetching data. Please try again later.</p>";
-    }
-  }
-  
-  // Fetch and show results based on url for searching
-  async function fetchAndShowSearch(url) {
-    const data = await fetchDataSearch(url);
-    if (data && data.results) {
-      showSearchResults(data.results);
-    }
-  }
-  
-  function showSearchResults(item) {
-    const searchContent = item
-      .map((media) => {
-        if (media.media_type === "movie") {
-          return createSearchCard(media);
-        } else if (media.media_type === "tv") {
-          return createSearchCardTv(media);
-        }
-      })
-      .join("");
-  
-    /*Check if "No results found" already exists so it doesnt show multiple times*/
-    if (searchContent == "") {
-      isLoadingMore = true;
-      if (!searchResult.innerHTML.includes("No results found.")) {
-        searchResult.innerHTML += "<p>No results found.</p>";
-        loader.classList.add("hide-element")
-      }
-    } else {
-      searchResult.innerHTML += searchContent;
-    }
-  }
-  
-  /******Importing genres from genres.js ******/
-  import { movieGenres, tvGenres } from "./genres.js";
-  
-  function createSearchCard(movie) {
-    const {
-      backdrop_path,
-      poster_path,
-      original_title,
-      release_date,
-      overview,
-      id,
-      media_type,
-      genre_ids,
-    } = movie;
-  
-    const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
-    const imagePath = poster_path ? imgApi + poster_path : "./images/no_image.svg";
-  
-    const searchTitle =
-      original_title.length > 25
-        ? original_title.slice(0, 25) + "..."
-        : original_title;
-    const formattedDate =
-      release_date.length > 4
-        ? release_date.slice(0, 4)
-        : release_date || "N/A";
-    const year = formattedDate.slice(0, 4);
-    const escapedOverview = overview.replace(/['"]/g, "&apos;");
-    const description =
-      escapedOverview.length > 110
-        ? escapedOverview.slice(0, 110) + "... "
-        : escapedOverview;
-  
-    // Get genre_ids from api and Map genre IDs to their names which is located in genres.js
-    const genreNames = genre_ids
-      .map((genreId) => {
-        const genre = movieGenres.find((genre) => genre.id === genreId);
-        return genre ? genre.name : "N/A";
-      })
-      .join(", ");
-  
-    const cardTemplate = `
+    })
+    .join("");
+
+  searchResult.innerHTML += searchContent || "No results found";
+}
+
+/******Importing genres from genres.js ******/
+import { movieGenres, tvGenres } from "./genres.js";
+
+function createSearchCard(movie) {
+  const {
+    backdrop_path,
+    poster_path,
+    original_title,
+    release_date,
+    overview,
+    id,
+    media_type,
+    genre_ids,
+  } = movie;
+
+  const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
+  const imagePath = poster_path
+    ? imgApi + poster_path
+    : "./images/no_image.svg";
+
+  const searchTitle =
+    original_title.length > 25
+      ? original_title.slice(0, 25) + "..."
+      : original_title;
+  const formattedDate =
+    release_date.length > 4 ? release_date.slice(0, 4) : release_date || "N/A";
+  const year = formattedDate.slice(0, 4);
+  const escapedOverview = overview.replace(/['"]/g, "&apos;");
+  const description =
+    escapedOverview.length > 110
+      ? escapedOverview.slice(0, 110) + "... "
+      : escapedOverview;
+
+  // Get genre_ids from api and Map genre IDs to their names which is located in genres.js
+  const genreNames = genre_ids
+    .map((genreId) => {
+      const genre = movieGenres.find((genre) => genre.id === genreId);
+      return genre ? genre.name : "N/A";
+    })
+    .join(", ");
+
+  const cardTemplate = `
     <a href="info.html">
               <div class="search-card" data-id="${id}">
                 <img src="${imagePath}" alt="" />
@@ -128,49 +127,51 @@ async function fetchDataSearch(url) {
     </a>
 
         `;
-  
-    return cardTemplate;
-  }
-  
-  function createSearchCardTv(tv) {
-    const {
-      backdrop_path,
-      poster_path,
-      original_name,
-      first_air_date,
-      overview,
-      id,
-      media_type,
-      genre_ids,
-    } = tv;
-  
-    const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
-    const imagePath = poster_path ? imgApi + poster_path : "./images/no_image.svg";
-  
-    const searchTitle =
-      original_name.length > 31
-        ? original_name.slice(0, 29) + "..."
-        : original_name;
-    const formattedDate =
-      first_air_date.length > 4
-        ? first_air_date.slice(0, 4)
-        : first_air_date || "N/A";
-    const year = formattedDate.slice(0, 4);
-    const escapedOverview = overview.replace(/['"]/g, "&apos;");
-    const description =
-      escapedOverview.length > 110
-        ? escapedOverview.slice(0, 110) + "... "
-        : escapedOverview;
-  
-    // Get genre_id from api and Map genre IDs to their names which are located in genres.js
-    const genreNames =  genre_ids
-      .map((genreId) => {
-        const genre = tvGenres.find((genre) => genre.id === genreId);
-        return genre ? genre.name : "";
-      })
-      .join(", ");
-  
-    const cardTemplate = `
+
+  return cardTemplate;
+}
+
+function createSearchCardTv(tv) {
+  const {
+    backdrop_path,
+    poster_path,
+    original_name,
+    first_air_date,
+    overview,
+    id,
+    media_type,
+    genre_ids,
+  } = tv;
+
+  const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
+  const imagePath = poster_path
+    ? imgApi + poster_path
+    : "./images/no_image.svg";
+
+  const searchTitle =
+    original_name.length > 31
+      ? original_name.slice(0, 29) + "..."
+      : original_name;
+  const formattedDate =
+    first_air_date.length > 4
+      ? first_air_date.slice(0, 4)
+      : first_air_date || "N/A";
+  const year = formattedDate.slice(0, 4);
+  const escapedOverview = overview.replace(/['"]/g, "&apos;");
+  const description =
+    escapedOverview.length > 110
+      ? escapedOverview.slice(0, 110) + "... "
+      : escapedOverview;
+
+  // Get genre_id from api and Map genre IDs to their names which are located in genres.js
+  const genreNames = genre_ids
+    .map((genreId) => {
+      const genre = tvGenres.find((genre) => genre.id === genreId);
+      return genre ? genre.name : "";
+    })
+    .join(", ");
+
+  const cardTemplate = `
 
     <a href="info.html">
 
@@ -190,81 +191,78 @@ async function fetchDataSearch(url) {
 
   
   `;
-  
-    return cardTemplate;
+
+  return cardTemplate;
+}
+
+// Clear result element for search
+function clearResults() {
+  //Checks if the page is on index.html before clearing content
+  searchResult.innerHTML = "";
+  trendingResult.innerHTML = "";
+  topRatedTvResult.innerHTML = "";
+  upcomingMoviesResult.innerHTML = "";
+  page = 1;
+  totalResultsCount = 0;
+}
+
+// Handle search
+async function handleSearch(e) {
+  e.preventDefault();
+  const searchTermInput = query.value.trim();
+
+  if (searchTermInput) {
+    isSearching = true;
+    clearResults();
+    searchSkeleton.classList.remove("hide-element");
+    searchTerm = searchTermInput;
+    const newUrl = `${searchUrl}${searchTerm}`;
+    const sectionHeadline = document.querySelectorAll(".sectionHeadline");
+    sectionHeadline.forEach((headline) => {
+      headline.classList.add("hide-element");
+    });
+    const resultsText = document.getElementById("resultsText");
+
+    trendingResult.classList.add("hide-element");
+    popularMoviesResult.classList.add("hide-element");
+    topRatedTvResult.classList.add("hide-element");
+    upcomingMoviesResult.classList.add("hide-element");
+
+    searchResult.classList.remove("hide-element");
+    // loadMoreBtn.classList.remove("hide-element");
+    resultsText.classList.remove("hide-element");
+    resultsText.innerText = "Showing results for: " + query.value;
+    query.value = "";
+
+    await fetchAndShowSearch(newUrl);
   }
-  
-  // Clear result element for search
-  function clearResults() {
-    //Checks if the page is on index.html before clearing content
-    if (document.location.pathname.includes("index.html")) {
-        trendingResult.innerHTML = "";
-        topRatedTvResult.innerHTML = "";
-        upcomingMoviesResult.innerHTML = "";
-      }
-    searchResult.innerHTML = "";
-    page = 1;
-    isLoadingMore = false;
-  }
-  
-  // Handle search
-  async function handleSearch(e) {
-    e.preventDefault();
-    const searchTermInput = query.value.trim();
-  
-    if (searchTermInput) {
-      isSearching = true;
-      clearResults();
-      searchTerm = searchTermInput;
-  
-      const newUrl = `${searchUrl}${searchTerm}`;
-      const sectionHeadline = document.querySelectorAll(".sectionHeadline");
-      sectionHeadline.forEach((headline) => {
-        headline.classList.add("hide-element");
-      });
-      const resultsText = document.getElementById("resultsText");
+}
 
-
-          trendingResult.classList.add("hide-element");
-          popularMoviesResult.classList.add("hide-element");
-          topRatedTvResult.classList.add("hide-element");
-          upcomingMoviesResult.classList.add("hide-element");       
-
-      searchResult.classList.remove("hide-element");
-      // loadMoreBtn.classList.remove("hide-element");
-      resultsText.classList.remove("hide-element");
-      resultsText.innerText = "Showing results for: " + query.value;
-      query.value = "";
-      await fetchAndShowSearch(newUrl);
+// Load more results
+function loadMoreResults() {
+  if (isSearching) {
+    if (searchTerm) {
+      page++;
+      const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
+      searchSkeleton.classList.remove("hide-element");
+      fetchAndShowSearch(newUrl);
     }
   }
-  
-  // Load more results
-  function loadMoreResults() {
-    if (isSearching) {
-      if (searchTerm) {
-        page++;
-        const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
-        loader.classList.remove("hide-element")
-        setTimeout(() => {
-         fetchAndShowSearch(newUrl); 
-          loader.classList.add("hide-element");
-          isLoadingMore = false;
-        }, 1000);
-        
-      }
-    }
-  }
-  
-  // Detect end of page and load more results
-  function detectEnd() {
+}
+
+// Detect end of page and load more results
+function detectEnd() {
+  //if results that are loaded in is not the same as the results available from api
+  if (totalResultsCount != totalSearchResults) {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 100 && !isLoadingMore) {
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
       loadMoreResults();
     }
   }
-  
-  // Event listeners
-  form.addEventListener("submit", handleSearch);
-  // loadMoreBtn.addEventListener("click", loadMoreResults)
-  window.addEventListener("scroll", detectEnd);
+}
+
+// Event listeners
+form.addEventListener("submit", handleSearch);
+// loadMoreBtn.addEventListener("click", loadMoreResults)
+
+window.addEventListener("scroll", detectEnd);
