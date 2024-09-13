2795ef4898a09c68c2f150ea18681ab9a9c40696
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2024-09-13 12:04:44 +0200

    fixed how the rating was shown in infopage. also added runtime and genres

--- src/movieInfo.js
+++ src/movieInfo.js
@@ -1,66 +1,62 @@
-import {apiKey, imgApi} from "./main.js";
-
+import { apiKey, imgApi } from "./main.js";
 
 const infoSectionContainer = document.getElementById("info-section-container");
 
 // Fetch JSON data from url
 async function fetchInfoDataMovie(url) {
-    try {
-      const response = await fetch(url);
-      console.log(url);
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      const data = await response.json();
-      console.log(data);
-      if (data) {
-        // infoSectionContainer.innerHTML = "";
-        showResults(data, "movie");
-      }
-    } catch (error) {
-      return null;
+  try {
+    const response = await fetch(url);
+    console.log(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
     }
+    const data = await response.json();
+    console.log(data);
+    if (data) {
+      // infoSectionContainer.innerHTML = "";
+      showResults(data, "movie");
+    }
+  } catch (error) {
+    return null;
   }
+}
 
-  // Fetch JSON data from url
+// Fetch JSON data from url
 async function fetchInfoDataTv(url) {
-    try {
-      const response = await fetch(url);
-      console.log(url);
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      const data = await response.json();
-      console.log(data);
-      if (data) {
-        // infoSectionContainer.innerHTML = "";
-        showResults(data, "tv");
-      }
-    } catch (error) {
-      return null;
+  try {
+    const response = await fetch(url);
+    console.log(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
     }
+    const data = await response.json();
+    console.log(data);
+    if (data) {
+      // infoSectionContainer.innerHTML = "";
+      showResults(data, "tv");
+    }
+  } catch (error) {
+    return null;
   }
+}
 
-  async function fetchInfoMovie(url) {
-    await fetchInfoDataMovie(url);
+async function fetchInfoMovie(url) {
+  await fetchInfoDataMovie(url);
+}
+
+async function fetchInfoTv(url) {
+  await fetchInfoDataTv(url);
+}
+
+function showResults(item, mediaType) {
+  if (mediaType === "movie") {
+    const infoContent = createInfoPageMovie(item);
+    infoSectionContainer.innerHTML = infoContent || "<p>No results found<p>";
+  } else if (mediaType === "tv") {
+    const infoContent = createInfoPageTv(item);
+    infoSectionContainer.innerHTML = infoContent || "<p>No results found<p>";
   }
-  
-  async function fetchInfoTv(url) {
-    await fetchInfoDataTv(url);
-  }
-  
-  
-  
-  function showResults(item, mediaType) {
-    if (mediaType === "movie") {
-      const infoContent = createInfoPageMovie(item);
-      infoSectionContainer.innerHTML = infoContent || "<p>No results found<p>";
-    } else if (mediaType === "tv") {
-      const infoContent = createInfoPageTv(item);
-      infoSectionContainer.innerHTML = infoContent || "<p>No results found<p>";
-    }
-  }
-  
+}
 
 /******Importing genres from genres.js ******/
 import { movieGenres, tvGenres } from "./genres.js";
@@ -77,7 +73,7 @@ function createInfoPageMovie(movie) {
     genres,
     vote_average,
     vote_count,
-    runtime
+    runtime,
   } = movie;
 
   const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
@@ -85,23 +81,20 @@ function createInfoPageMovie(movie) {
     ? imgApi + poster_path
     : "./images/no_image.svg";
 
-  const searchTitle =
-    original_title.length > 25
-      ? original_title.slice(0, 25) + "..."
-      : original_title;
-  const formattedDate =
-    release_date.length > 4 ? release_date.slice(0, 4) : release_date || "N/A";
-  const year = formattedDate.slice(0, 4);
-  const escapedOverview = overview.replace(/['"]/g, "&apos;");
-  const description =
-    escapedOverview.length > 110
-      ? escapedOverview.slice(0, 110) + "... "
-      : escapedOverview;
+  const year = release_date.slice(0, 4) || "N/A";
+  const rating = vote_average.toString().slice(0, 3) || "N/A";
 
-  // Get genre_ids from api and Map genre IDs to their names which is located in genres.js
+  const runtimeHour = runtime > 60 ? runtime / 60 : "0";
+  const runtimeMinute = runtime > 60 ? runtime % 60 : "0";
+
+  const formatRuntime =
+    runtimeHour.toString().slice(0, 1) +
+    "h " +
+    runtimeMinute.toString().slice(0, 2) +
+    "m ";
+
   const genreNames = genres
-    .map((genreId) => {
-      const genre = movieGenres.find((genre) => genre.id === genreId);
+    .map((genre) => {
       return genre ? genre.name : "N/A";
     })
     .join(", ");
@@ -121,14 +114,16 @@ function createInfoPageMovie(movie) {
           <div class="info-content">
             <div class="title_general-info">
               <div class="info-title">
-                <h1>${original_title}<span class="year"${year}</span></h1>
+                <h1>${original_title} <span class="year">(${year})</span></h1>
                 
               </div>
               <div class="general-info">
                 <div class="rating">
-                  <p class="star"><i class="fa-solid fa-star" style="color: #FFD43B;"></i>${vote_average}</p>
+                  <p class="star"><i class="fa-solid fa-star" style="color: #FFD43B;"></i>${rating}</p>
                 </div>
-                <div class="runtime">${runtime}m</div>
+                •
+                <div class="runtime">${formatRuntime}</div>
+                •
                 <div class="genre">${genreNames}</div>
               </div>
             </div>
@@ -158,7 +153,7 @@ function createInfoPageTv(tv) {
     media_type,
     genres,
     vote_average,
-    vote_count
+    vote_count,
   } = tv;
 
   const backDrop = backdrop_path ? imgApi + backdrop_path : "./img-01.jpeg";
@@ -167,13 +162,10 @@ function createInfoPageTv(tv) {
     : "./images/no_image.svg";
 
   const year = first_air_date.slice(0, 4);
+  const rating = vote_average.toString().slice(0, 3) || "N/A";
 
-
-  
-  // Get genre_id from api and Map genre IDs to their names which are located in genres.js
   const genreNames = genres
-    .map((genreId) => {
-      const genre = tvGenres.find((genre) => genre.id === genreId);
+    .map((genre) => {
       return genre ? genre.name : "N/A";
     })
     .join(", ");
@@ -199,8 +191,9 @@ function createInfoPageTv(tv) {
               </div>
               <div class="general-info">
                 <div class="rating">
-                  <p class="star"><i class="fa-solid fa-star" style="color: #FFD43B;"></i>${vote_average}</p>
+                  <p class="star"><i class="fa-solid fa-star" style="color: #FFD43B;"></i>${rating}</p>
                 </div>
+                •
                 <div class="genre">${genreNames}</div>
               </div>
             </div>
@@ -221,19 +214,16 @@ function createInfoPageTv(tv) {
 }
 //get media ID and mediaType from the url
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
-const mediaType = urlParams.get('mediaType');
+const id = urlParams.get("id");
+const mediaType = urlParams.get("mediaType");
 
 initInfoPage(id, mediaType);
 async function initInfoPage(id, mediaType) {
-    if(mediaType === "tv"){
-        const tvInfoUrl = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`
-        await fetchInfoTv(tvInfoUrl);
-    }
-    else if(mediaType === "movie"){
-        const movieInfoUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
-        await fetchInfoMovie(movieInfoUrl);
-    }
+  if (mediaType === "tv") {
+    const tvInfoUrl = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`;
+    await fetchInfoTv(tvInfoUrl);
+  } else if (mediaType === "movie") {
+    const movieInfoUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+    await fetchInfoMovie(movieInfoUrl);
   }
-  
-
+}
